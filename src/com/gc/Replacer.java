a3e54d67867620e351b1ff4de9abe56bd08c7317
James <j@sokoll.co>
2015-04-08 15:54:41 -0400

    none

--- src/com/gc/Replacer.java
+++ src/com/gc/Replacer.java
@@ -5,12 +5,7 @@ public class Replacer {
 	public static Holder replaceTargetsWithTranslations(Holder ourHolder) {
 		String[] targets = ourHolder.getTargets();
 		String[] translations = ourHolder.getTranslations();
-		System.out.println("array length=" + translations.length);
-		
 		StringBuilder body = new StringBuilder(ourHolder.getBody());
-		System.out.println("body length=" + body.length());
-		
-		System.out.println("length="+ translations.length);
 		
 		for (int i = 0; i < translations.length; i++) {
 			translations[i] = "<a href=\"#\" class=\"tip\" title=\"" + targets[i] + "\">" + translations[i] + "</a>";
